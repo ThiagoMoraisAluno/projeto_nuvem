92822f6d9b3a51c9d62c656efbb95863d26d3654
Thiago Morais Cordeiro <thiagomoraiscordeiro@outlook.com>
2023-01-15 16:16:04 -0300

    criação das pastas pages e partials e fazendo a separação de páginas

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    'recipes',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
