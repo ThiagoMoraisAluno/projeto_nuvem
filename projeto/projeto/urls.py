b52c7374faa22f9272968e25b0d6b05d0e1ffe75
Thiago Morais Cordeiro <thiagomoraiscordeiro@outlook.com>
2023-01-20 17:34:38 -0300

    criando a nova página de descrição da receita

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from recipes.views import home
+
+from recipes.views import home, recipe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home, name='home'),
+    path('recipe/<int:id>/', recipe, name='recipe'),
 ]
